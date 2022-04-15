bd1a891d8b58919c6d199571301b3040c214cbf8
Pedro Botsaris <pedro@antfood.com>
2022-04-15 15:59:34 +0200

    moved factory to dedicated header

---
+++ include/factory.h
@@ -0,0 +1,69 @@
+#ifndef FACTORY_H
+#define FACTORY_H
+#include "Operands.h"
+
+struct FactoryFunctions
+{
+
+    static IOperand* createInt8(const std::string& value)
+    {
+        auto val = static_cast<int8_t>(std::stoi(value));
+        return new Operands<int8_t>(val);
+    }
+
+    static IOperand* createInt16(const std::string& value)
+    {
+        auto val = static_cast<int16_t>(std::stoi(value));
+        return new Operands<int16_t>(val);
+    }
+
+    static IOperand* createInt32(const std::string& value)
+    {
+        auto val = static_cast<int32_t>(std::stoi(value));
+        return new Operands<int32_t>(val);
+    }
+
+    static IOperand* createFloat(const std::string& value)
+    {
+        float val = std::stof(value);
+        return new Operands<float>(val);
+    }
+
+    static IOperand* createDouble(const std::string& value)
+    {
+        double val = std::stod(value);
+        return new Operands<double>(val);
+    }
+
+};
+
+struct Factory
+{
+private:
+    std::map<eOperandType, FactoryFunction> factory;
+
+    Factory()
+    {
+        factory.insert(std::pair<eOperandType, FactoryFunction>(Int8_t, &FactoryFunctions::createInt8));
+        factory.insert(std::pair<eOperandType, FactoryFunction>(Int16_t, &FactoryFunctions::createInt16));
+        factory.insert(std::pair<eOperandType, FactoryFunction>(Int32_t, &FactoryFunctions::createInt32));
+        factory.insert(std::pair<eOperandType, FactoryFunction>(Float_t, &FactoryFunctions::createFloat));
+        factory.insert(std::pair<eOperandType, FactoryFunction>(Float_t, &FactoryFunctions::createDouble));
+    };
+
+public:
+    static Factory& getInstance()
+    {
+        static Factory instance = Factory();
+        return instance;
+    };
+
+    Factory(Factory const&) = delete;              // Don't Implement
+    void operator=(Factory const&) = delete;
+
+    IOperand* create(eOperandType type, const std::string& value)
+    {
+        return factory.at(type)(value);
+    };
+};
+#endif
--- src/operands.cpp
+++ src/operands.cpp
@@ -1,77 +1,8 @@
-#include "../include/Operands.h"
+#include "../include/factory.h"
 
 /* Constructor & Destructor */
 
-struct FactoryFunctions 
-{
-
-    static IOperand* createInt8(const std::string& value)
-     {
-         int8_t val = std::stoi(value);
-         return new Operands<int8_t>(val);
-     }
-
-     static IOperand* createInt16(const std::string& value)
-     {
-         int16_t val = std::stoi(value);
-         return new Operands<int16_t>(val);
-     }
-
-     static IOperand* createInt32(const std::string& value)
-     {
-         int32_t val = std::stoi(value);
-         return new Operands<int32_t>(val);
-     }
-
-     static IOperand* createFloat(const std::string& value)
-     {
-         float val = std::stof(value);
-         return new Operands<float>(val);
-     }
-
-     static IOperand* createDouble(const std::string& value)
-     {
-      double val = std::stod(value);
-      return new Operands<double>(val);
-     }
-
-};
-
-struct Factory
-{
-    private:
-      std::map<eOperandType, FactoryFunction> factory;
-
-
-       Factory()
-       {
-        factory.insert(std::pair<eOperandType, FactoryFunction>(Int8_t, &FactoryFunctions::createInt8));
-        factory.insert(std::pair<eOperandType, FactoryFunction>(Int16_t, &FactoryFunctions::createInt16));
-        factory.insert(std::pair<eOperandType, FactoryFunction>(Int32_t, &FactoryFunctions::createInt32));
-        factory.insert(std::pair<eOperandType, FactoryFunction>(Float_t, &FactoryFunctions::createFloat));
-        factory.insert(std::pair<eOperandType, FactoryFunction>(Float_t, &FactoryFunctions::createDouble));
-       };
-
-        Factory(Factory const&);              // Don't Implement
-        void operator=(Factory const&);
-
-  public:
-       static Factory& getInstance() 
-       { 
-         static Factory instance = Factory(); 
-         return instance;
-       };
-
-       IOperand* create(eOperandType type, const std::string& value)
-       {
-         return factory.at(type)(value);
-       };
-};
-
-
-Factory& factory = Factory::getInstance();
-
-
+static Factory& factory = Factory::getInstance();
 
 template <typename T>
   Operands<T>::Operands(T value)
@@ -81,7 +12,7 @@ template <typename T>
   };
 
 template <typename T>
-  Operands<T>::~Operands() {};
+  Operands<T>::~Operands() = default;
 
 /* Public Implementation */
 
@@ -89,64 +20,63 @@ template <typename T>
   eOperandType Operands<T>::getType() const 
 {
   return m_type;
-};
+}
 
 template <typename T>
   int Operands<T>::getPrecision() const 
 {
      return m_type;
-};
+}
 
 template <typename T>
   const std::string& Operands<T>::toString() const
   {
      return m_value;
-  };
+  }
 
 template<typename T>
   IOperand* Operands<T>::operator+(const IOperand &rhs) const
 {
     eOperandType return_type = std::max(m_type, rhs.getType());
     T left_side              = Private::convert(*this);
-    auto right_side          = rhs.getType() >= FLOAT_INDEX  ? std::stod(rhs.toString()) : std::stoi(rhs.toString());
+    auto right_side  = rhs.getType() >= FLOAT_INDEX  ? std::stod(rhs.toString()) : std::stoi(rhs.toString());
     std::string result       = std::to_string(left_side + right_side);
 
     return factory.create(return_type, result);
-};
+}
 
 template<typename T>
   IOperand* Operands<T>::operator-(const IOperand &rhs) const
 {
     eOperandType return_type = std::max(m_type, rhs.getType());
     T left_side              = Private::convert(*this);
-    auto right_side          = rhs.getType() >= FLOAT_INDEX  ? std::stod(rhs.toString()) : std::stoi(rhs.toString());
+    auto right_side  = rhs.getType() >= FLOAT_INDEX  ? std::stod(rhs.toString()) : std::stoi(rhs.toString());
     std::string result       = std::to_string(left_side - right_side);
 
     return factory.create(return_type, result);
-};
+}
 
 template<typename T>
   IOperand* Operands<T>::operator*(const IOperand &rhs) const
 {
     eOperandType return_type = std::max(m_type, rhs.getType());
     T left_side              = Private::convert(*this);
-    auto right_side          = rhs.getType() >= FLOAT_INDEX  ? std::stod(rhs.toString()) : std::stoi(rhs.toString());
+    auto right_side  = rhs.getType() >= FLOAT_INDEX  ? std::stod(rhs.toString()) : std::stoi(rhs.toString());
     std::string result       = std::to_string(left_side * right_side);
 
     return factory.create(return_type, result);
-};
-
+}
 
 template<typename T>
   IOperand* Operands<T>::operator/(const IOperand &rhs) const
 {
     eOperandType return_type = std::max(m_type, rhs.getType());
     T left_side              = Private::convert(*this);
-    auto right_side          = rhs.getType() >= FLOAT_INDEX  ? std::stod(rhs.toString()) : std::stoi(rhs.toString());
+    auto right_side  = rhs.getType() >= FLOAT_INDEX  ? std::stod(rhs.toString()) : std::stoi(rhs.toString());
     std::string result       = std::to_string(left_side / right_side);
 
     return factory.create(return_type, result);
-};
+}
 
 
 template<typename T>
@@ -154,12 +84,11 @@ template<typename T>
 {
     eOperandType return_type = std::max(m_type, rhs.getType());
     T left_side              = Private::convert(*this);
-    auto right_side          = rhs.getType() >= FLOAT_INDEX  ? std::stod(rhs.toString()) : std::stoi(rhs.toString());
+    auto right_side  = rhs.getType() >= FLOAT_INDEX  ? std::stod(rhs.toString()) : std::stoi(rhs.toString());
     std::string result       = std::to_string(std::fmod(left_side, right_side));
 
     return factory.create(return_type, result);
-};
-
+}
 
 /* Private Implementation */
 
@@ -184,40 +113,47 @@ template <typename T>
         self.m_type = Double_t;
     }
 
-    static const T convert(const Operands& self) 
-{
-      if(std::is_same<T, float>::value)
-          return std::stof(self.m_value);
+    static T convert(const Operands &self)
+    {
+        if (std::is_same<T, float>::value)
+            return std::stof(self.m_value);
 
-      if(std::is_same<T, double>::value)
-        return std::stod(self.m_value);
+        if (std::is_same<T, double>::value)
+            return std::stod(self.m_value);
 
-      else
-       return std::stoi(self.m_value);
- };
+        else
+            return std::stoi(self.m_value);
+    }
 
   };
 
-
-
-
 /* TESTS */
 
-TEST_CASE("Operands: Public methods")
+TEST_CASE("Operands")
 {
- auto i8  = Operands<int8_t>(2);
- auto i8_two = Operands<int8_t>(3);
+    SUBCASE("create very type")
+    {
+        auto i8 = Operands<int8_t>(10);
+        auto i16 = Operands<int16_t>(20);
+        auto i32 = Operands<int32_t>(100);
+        auto f = Operands<float>(30.20);
+        auto d = Operands<double>(40.2020);
 
- auto res = i8 + i8_two;
+        CHECK(i8.toString() == "10");
+        CHECK(i16.toString() == "20");
+        CHECK(i1.toString() == "20");
 
- CHECK(res->toString() == "5");
+    }
 
- delete res;
+    SUBCASE("same type")
+    {
+        auto i8 = Operands<int8_t>(2);
+        auto i8_two = Operands<int8_t>(3);
 
- //auto i16 = Operands<int16_t>(200);
- //auto i32 = Operands<int32_t>(200);
- //auto f   = Operands<float>(10.20);
- //auto d   = Operands<double>(10.20);
+        auto res = i8 + i8_two;
+        CHECK(res->toString() == "5");
+        delete res;
+    }
 
 }
 
